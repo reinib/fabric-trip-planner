58acf4ebf4830a161233e889f074c91743232edb
reinib <brent.reininger89@gmail.com>
2019-07-15 16:34:40 -0700

    completed task app

---
+++ src/TaskManager.js
@@ -0,0 +1,116 @@
+import { PersonaInitialsColor } from "office-ui-fabric-react";
+
+const dateOptions = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric"
+};
+
+let yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+const yesterdaysDate = yesterday.toLocaleDateString("en-us".dateOptions);
+
+export default class TaskManager {
+  constructor() {
+    this._tasks = [
+      {
+        id: 1,
+        title: "Wash the car",
+        completed: true,
+        personaProps: {
+          text: "Carol Poland",
+          secondaryText: `Created ${yesterdaysDate}`,
+          initialsColor: PersonaInitialsColor.darkRed,
+          imageUrl:
+            "https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-female.png"
+        }
+      },
+      {
+        id: 2,
+        title: "Get groceries",
+        completed: true,
+        personaProps: {
+          text: "Amanda Brady",
+          secondaryText: `Created ${yesterdaysDate}`,
+          initialsColor: PersonaInitialsColor.orange,
+          imageUrl:
+            "https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-female.png"
+        }
+      },
+      {
+        id: 3,
+        title: "Walk the dog",
+        completed: false,
+        personaProps: {
+          text: "Miguel Garcia",
+          secondaryText: `Created ${yesterdaysDate}`,
+          initialsColor: PersonaInitialsColor.blue,
+          imageUrl:
+            "https://static2.sharepointonline.com/files/fabric/office-ui-fabric-react-assets/persona-male.png"
+        }
+      }
+    ];
+  }
+
+  getTasks() {
+    return this._tasks;
+  }
+
+  getCompletedtaskCount() {
+    return this._tasks.filter(task => task.completed).length;
+  }
+
+  getTaskCount() {
+    return this._tasks.length;
+  }
+
+  getTaskPercentComplete() {
+    return this.getCompletedtaskCount() / this.getTaskCount();
+  }
+
+  addTask(title) {
+    let today = new Date();
+    let todaysDate = today.toLocaleString("en-us", dateOptions);
+
+    if (title) {
+      const newTask = {
+        id: this._tasks.length + 1,
+        title: title,
+        completed: false,
+        personaProps: {
+          text: "Brent Reininger",
+          secondaryText: `Created ${todaysDate}`,
+          initialsColor: PersonaInitialsColor.blue
+        }
+      };
+      this._tasks = this._tasks.concat(newTask);
+    }
+  }
+
+  toggleTaskCompleted(taskId) {
+    const updatedTasks = this._tasks;
+
+    updatedTasks.forEach((task, index) => {
+      let { id, completed } = task;
+
+      if (id === taskId) {
+        updatedTasks[index].completed = !completed;
+      }
+    });
+    this._tasks = updatedTasks;
+  }
+
+  deleteTask(taskId) {
+    let updatedTasks = this._tasks.slice();
+
+    updatedTasks.forEach((task, index) => {
+      let { id } = task;
+      if (id === taskId) {
+        updatedTasks.splice(index, 1);
+      }
+    });
+    this._tasks = updatedTasks;
+  }
+}
